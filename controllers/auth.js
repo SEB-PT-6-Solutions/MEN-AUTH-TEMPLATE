85c498d0b51dfee26bfeb21e95cc971f4f13cf9b
Denis S Dujota <denisdujota@gmail.com>
2025-09-08 20:10:53 +0300

    registaration complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
 const router = express.Router();
 
 router.get('/sign-up', (req, res) => {
   res.render('auth/sign-up.ejs');
+});
+
+router.post('/sign-up', async (req, res) => {
+  const userInDatabase = await User.findOne({ username: req.body.username });
+
+  if (userInDatabase) {
+    return res.send('Username or Password is invalid');
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return res.send('Password and Confirm Password must match');
+  }
+
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  req.body.password = hashedPassword;
+
+  const newUser = await User.create(req.body);
+  res.send(newUser);
 });
 
 module.exports = router;
